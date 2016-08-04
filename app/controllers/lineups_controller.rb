bcc8ff6c0aa5496e3641d2eba13bb1fa6abd388d
lorenzo892 <lorenzo.gimeno.ruiz@gmail.com>
2016-08-04 12:16:50 +0200

    not yet saved lineups in DB

--- app/controllers/lineups_controller.rb
+++ app/controllers/lineups_controller.rb
@@ -3,4 +3,15 @@ class LineupsController < ApplicationController
     @players_list= PlayerList.find_by(id: params[:players_list_id])
     @players=@players_list.players
   end
+  def create
+    my_partido = Partido.find_by(id: params[:partido_id])
+    my_player_list = PlayerList.find_by(id: params[:players_list_id])
+    players_list = Player.where(id: [params[:idPlayers]])
+    my_partido.player_list.lineup = Lineup.new
+    my_partido.player_list.lineup.players = players_list
+
+    render json: { redirect_to: url_for(action: :index) }, status: :ok
+
+  end
+
 end
